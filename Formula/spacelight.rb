6e5f11bf8aa749d50626377569b067d0b0339bd5
David <fzysmile@gmail.com>
2026-08-17 18:13:49 -0700

    add spacelight formular

---
+++ Formula/spacelight.rb
@@ -0,0 +1,24 @@
+class Spacelight < Formula
+  desc "Spotlight-style workspace and window switcher for AeroSpace"
+  homepage "https://github.com/zhenyufu/spacelight"
+  url "https://github.com/zhenyufu/spacelight.git",
+      tag:      "v0.1.0",
+      revision: "PLACEHOLDER"
+  license "MIT"
+
+  depends_on :macos
+  depends_on xcode: ["16.0", :build]
+
+  def install
+    system "swift", "build", "-c", "release", "--disable-sandbox"
+    bin.install ".build/release/Spacelight" => "spacelight"
+  end
+
+  def post_install
+    system bin/"spacelight", "quit" if (var/"run/spacelight").exist?
+  end
+
+  test do
+    assert_match "spacelight", shell_output("#{bin}/spacelight --help 2>&1", 1)
+  end
+end
